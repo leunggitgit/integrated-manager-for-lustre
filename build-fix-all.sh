6b49bcf3f5dadbd5e6195b8c3a2670abd9dd32b2
root <root@adm.local>
2022-05-18 01:09:02 +0000

    do not include Docker build for faster overall builds

--- build-fix-all.sh
+++ build-fix-all.sh
@@ -1,4 +1,5 @@
 #
+#
 # By Steve Leung
 #
 # This is the same as "make all".
@@ -8,11 +9,16 @@ make rpms > make-rpms.out 2>&1 \
 && make copr-rpms > make-copr-rpms.out 2>&1 \
 && make device-scanner-rpms > make-device-scanner-rpms.out 2>&1 \
 && make iml-gui-rpm > make-iml-gui-rpm.out 2>&1 \
-&& make docker-rpms > make-docker-rpms.out 2>&1 \
 && find _topdir
+
+#
+# Commented out Docker build for now. It takes too long to build, and we do not do Docker.
+#
+#&& make docker-rpms > make-docker-rpms.out 2>&1 \
 
 echo
 echo "Looking for errors..."
 echo
-fgrep -i ERROR make-rpms.out make-copr-rpms.out make-device-scanner-rpms.out make-iml-gui-rpm.out make-docker-rpms.out
+#fgrep -i ERROR make-rpms.out make-copr-rpms.out make-device-scanner-rpms.out make-iml-gui-rpm.out make-docker-rpms.out
+fgrep -i ERROR make-rpms.out make-copr-rpms.out make-device-scanner-rpms.out make-iml-gui-rpm.out 
 
